515a882608596fe5590daae897db56870a8667a7
rene ruben vicente <rene.vicente@ucb.edu.bo>
2022-09-03 16:56:57 -0400

    Añadiendo Spring al repo

---
+++ TallerSis/src/main/java/bo/ucb/edu/TallerSis/TallerSisApplication.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.TallerSis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerSisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerSisApplication.class, args);
+	}
+
+}
