f10570e64f84bfd6076642edee4b8410a89e6ae5
rene ruben vicente <rene.vicente@ucb.edu.bo>
2022-09-04 18:41:00 -0400

    Añadiendo BL

---
+++ TallerSis/src/main/java/bo/ucb/edu/TallerSis/DAO/UserDAO.java
@@ -0,0 +1,14 @@
+package bo.ucb.edu.TallerSis.DAO;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface UserDAO {
+
+
+}
