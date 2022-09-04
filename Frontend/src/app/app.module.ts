70bc0fabd9b83dcacdb4daef695329db91ef2705
Lindsay Uria <lindsay.karla.uria.valdivia@gmail.com>
2022-09-04 18:17:30 -0400

    Agregando login básico y archivos de carrito.

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RatingModule} from 'ng-starrating';
 import { FormsModule } from '@angular/forms';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CartPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/food';
+import { ActivatedRoute } from '@angular/router';
+import { CartPageComponent } from '../cart-page/cart-page.component';
+
 
 @Component({
   selector: 'app-home',
@@ -9,10 +12,16 @@ import { Foods } from '../shared/models/food';
 })
 export class HomeComponent implements OnInit {
   foods:Foods[]=[];
-  constructor(private fs:FoodService) { }
+  constructor(private fs:FoodService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.foods = this.fs.getAll();
+    this.route.params.subscribe(params =>{
+      if(params['searchItem'])
+      this.foods = this.fs.getAll().filter(food => food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
+      else
+      this.foods = this.fs.getAll();
+    })
+    
   }
 
 }
