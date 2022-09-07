5733e9429567071e3f357845d3e5f19867d118c7
Carlos Cavero <ccaverotx@gmail.com>
2022-09-07 02:30:33 -0400

    Added Angular Material for the Frontend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { RatingModule} from 'ng-starrating';
+//import { RatingModule} from 'ng-starrating';
 import { FormsModule } from '@angular/forms';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MapComponent } from './map/map.component';
 import { MapformComponent } from './mapform/mapform.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { MapformComponent } from './mapform/mapform.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
