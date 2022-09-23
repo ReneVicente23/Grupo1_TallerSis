1d3ff4f9e39886530db15fd11eca6911163d4a84
Lindsay Uria <lindsay.karla.uria.valdivia@gmail.com>
2022-09-23 03:17:24 -0400

    map form con funciones de api

--- Frontend/src/app/mapform/mapform.component.ts
+++ Frontend/src/app/mapform/mapform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MapformService } from '../services/mapform.service';
 
 @Component({
   selector: 'app-mapform',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapform.component.css']
 })
 export class MapformComponent implements OnInit {
+  address: any;
 
-  constructor() { }
+  title:string='REGISTRO DE DIRECCIONES';
+  constructor(private mapformService: MapformService) { }
 
   ngOnInit(): void {
+    this.retrieveAddress();
   }
+
+  retrieveAddress(): void {
+    this.mapformService.getAll()
+      .subscribe(
+        data => {
+          this.address = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  model:any = {};
+  model2:any = {};
+
+  saveAddress(): void {
+    const data = {
+      street: this.address.street,
+      zone: this.address.zone,
+      h_number: this.address.h_number,
+      city: this.address.city
+    };
+
+    this.mapformService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.refresh();
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  myValue:any;
+  refresh(): void { window.location.reload(); }
+
+
 
 }
