b33a7418efc630afc591188979aa9a36766240f9
Lindsay Uria <lindsay.karla.uria.valdivia@gmail.com>
2022-09-23 02:46:31 -0400

    llamando api address

--- Frontend/src/app/services/mapform.service.ts
+++ Frontend/src/app/services/mapform.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+const baseUrl = 'http://localhost:8080/api/user/address';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapformService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get<any>(baseUrl);
+  }
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+  delete(id: number): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
 }
