d05ef46e4daef1c24ac9d952267076db4db50ce2
Lindsay Uria <lindsay.karla.uria.valdivia@gmail.com>
2022-10-08 23:16:02 -0400

    Historial de Pedidos

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from "./components/pages/home/home.component";
 import { FoodPageComponent } from "./components/pages/food-page/food-page.component";
 import { CartPageComponent } from "./components/pages/cart-page/cart-page.component";
 import { SalesReportComponent } from './components/pages/sales-report/sales-report.component';
+import { OrdersHistoryComponent } from './components/pages/orders-history/orders-history.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path:'tag/:tag',component:HomeComponent},
   {path:'food/:id',component:FoodPageComponent},
   {path:'cart-page',component:CartPageComponent},
-  {path:'sales-report',component:SalesReportComponent}
+  {path:'sales-report',component:SalesReportComponent},
+  {path: 'orders-history', component:OrdersHistoryComponent}
 ];
 
 @NgModule({
