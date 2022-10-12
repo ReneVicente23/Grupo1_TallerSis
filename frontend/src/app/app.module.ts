4032a0b0f8fbe18a21aa9ec544db66fd36ad82a3
Lindsay Uria <lindsay.karla.uria.valdivia@gmail.com>
2022-10-12 00:09:14 -0400

    Agregar metodo de pago, pago en efectivo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,6 +29,9 @@ import { MapComponent } from './components/partials/map/map.component';
 import {AuthInterceptor} from "./auth/auth.interceptor";
 import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
 import { PaypalButtonComponent } from './components/partials/paypal-button/paypal-button.component';
+import { MessageConfirmComponent } from './components/pages/message-confirm/message-confirm.component';
+import { PendingPaymentsComponent } from './components/pages/pending-payments/pending-payments.component';
+import { PaymentConfirmationComponent } from './components/pages/payment-confirmation/payment-confirmation.component';
 
 @NgModule({
   declarations: [
@@ -52,7 +55,10 @@ import { PaypalButtonComponent } from './components/partials/paypal-button/paypa
     OrderItemsListComponent,
     MapComponent,
     PaymentPageComponent,
-    PaypalButtonComponent
+    PaypalButtonComponent,
+    MessageConfirmComponent,
+    PendingPaymentsComponent,
+    PaymentConfirmationComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/pages/pending-payments/pending-payments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-payments',
+  templateUrl: './pending-payments.component.html',
+  styleUrls: ['./pending-payments.component.css']
+})
+export class PendingPaymentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
